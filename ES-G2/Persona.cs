897ee74a5ed31c5b7baba8eab3e42ee0c5a841e3
Stefanospi <sspitaleri26@gmail.com>
2024-06-18 16:02:12 +0200

    ES-G2

---
+++ ES-G2/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_G2
+{
+    internal class Persona
+    {
+        public string _nome;
+        public string _cognome;
+        public int _eta;
+
+        public Persona(string nome,string cognome,int eta)
+        {  
+            _nome = nome;
+            _cognome = cognome;
+            _eta = eta;
+        }
+
+        public void getNome()
+        {
+            Console.WriteLine(_nome);
+        }
+        public void getCognome() { 
+            Console.WriteLine(_cognome);
+
+        }
+        public void getEta() {
+            Console.WriteLine(_eta);
+
+        }
+
+        public void getAllPersona()
+        {
+            Console.WriteLine($"{_nome} {_cognome} {_eta}");
+        }
+    }
+}
---
+++ ES-G2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ES_G2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Stefano","Spitaleri",23);
+
+            persona.getNome();
+            persona.getCognome();
+            persona.getEta();
+            persona.getAllPersona();
+
+        }
+    }
+}
